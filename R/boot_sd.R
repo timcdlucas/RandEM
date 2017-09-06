b06dca747915c0d117800d39c51b3aed9ac37744
Tim Lucas <timcdlucas@gmail.com>
2017-09-06 21:08:25 +0100

    Add tm and v as args to boot_sd. They were being pulled from the global env which is bad.

--- R/boot_sd.R
+++ R/boot_sd.R
@@ -1,7 +1,9 @@
 #' Required for remsD bootstrapping
 #' 
 #' Resamples camera trap data to calculate variance for Random Encounter Model density estimates. Note that tm and v must be the same across all sites. If this is not the case, split your data before bootstrapping and run the function on each sub-group. Called by remsD. Calls bsD.
-#' @param x = bsD resampled data
+#' @param x bsD resampled data
+#' @param tm (numeric) The total number of hours all cameras were left in-situ at a focal site
+#' @param v (numeric) The distance travelled by the focal species in 24 hours, in kilometres
 #' @export
 #' @examples 
 #' ## Define the number of bootstrapping iterations and apply boot_sd to the data:
@@ -16,7 +18,7 @@
 #' 
 #' @importFrom stats quantile sd
 
-boot_sd <-  function(x){
+boot_sd <-  function(x, tm, v){
   d <- replicate(nboots, bsD(x, tm, v)) 
   return(d)
 }
