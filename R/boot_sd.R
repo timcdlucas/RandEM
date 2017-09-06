c76a6ba0d0b77740c8195962a0ee4e1c2ab6e95b
Tim Lucas <timcdlucas@gmail.com>
2017-09-06 21:11:09 +0100

    Add nboots as sd_boot arg and fix example.

--- R/boot_sd.R
+++ R/boot_sd.R
@@ -4,6 +4,7 @@
 #' @param x bsD resampled data
 #' @param tm (numeric) The total number of hours all cameras were left in-situ at a focal site
 #' @param v (numeric) The distance travelled by the focal species in 24 hours, in kilometres
+#' @param nboots (numeric) The number of bootstrap iterations
 #' @export
 #' @examples 
 #' ## Define the number of bootstrapping iterations and apply boot_sd to the data:
@@ -12,13 +13,13 @@
 #' nboots <- 1000
 #' 
 #' grpDat <- split_dat(hDat)
-#' remsD <- lapply(grpDat, boot_sd)
+#' remsD <- lapply(grpDat, boot_sd, tm, v, nboots)
 #' remsSD <- lapply(remsD, sd)
 #' remsSD
 #' 
 #' @importFrom stats quantile sd
 
-boot_sd <-  function(x, tm, v){
+boot_sd <-  function(x, tm, v, nboots){
   d <- replicate(nboots, bsD(x, tm, v)) 
   return(d)
 }
