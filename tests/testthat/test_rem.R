96ada3e3b8f4598ade59235a0d90fadf9ff4802f
Tim Lucas <timcdlucas@gmail.com>
2018-05-26 20:45:11 +0100

    Convert hDat to km in rem test and change regression test.

--- tests/testthat/test_rem.R
+++ tests/testthat/test_rem.R
@@ -4,6 +4,7 @@ test_that("rem produces a reasonable result", {
   # This is just a very basic regression test
   
   data(hDat)
+  hDat$dist <- 1000 * hDat$dist
   grpDat <- split_dat(hDat)
   tm <- 3600
   v <- 1.4
@@ -15,40 +16,9 @@ test_that("rem produces a reasonable result", {
   
   # "Known" values. Taken from running `lapply(grpDat, rem, tm, v)` and saving
   # the values with `dput`
-  known_output <- c(2.24999473853356, 
-                    1.07668613384841, 
-                    1.86016465692493, 
-                    1.45899308473601, 
-                    1.52149811808646)
+  known_output <- c(53.99987, 25.84047, 44.64395, 35.01583, 36.51595)
   max_resid <- max(abs(unlist(densities) - known_output))
   
-  expect_true(max_resid < 6e-15)
+  expect_true(max_resid < 6e-3)
   
-  if(FALSE){
-    
-    data(hDat)
-    hDat$dist <- 1000 * hDat$dist
-    grpDat <- split_dat(hDat)
-    tm <- 3600
-    v <- 1.4
-    
-    # Test an example output against known outputs and check that the difference
-    densities <- lapply(grpDat, rem, tm, v)
-    expect_is(densities, "list")
-    expect_length(densities, 5)
-    
-    # "Known" values. Taken from running `lapply(grpDat, rem, tm, v)` and saving
-    # the values with `dput`
-    known_output <- c(2.24999473853356, 
-                      1.07668613384841, 
-                      1.86016465692493, 
-                      1.45899308473601, 
-                      1.52149811808646)
-    max_resid <- max(abs(unlist(densities) - known_output))
-    
-    expect_true(max_resid < 6e-15)
-    
-    
-    
-  }
 })
