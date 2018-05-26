33e2bdbc48ff8ef30a0d9016a0994b5cd491b612
Tim Lucas <timcdlucas@gmail.com>
2018-05-26 20:02:11 +0100

    convert hDat column to metres in test and redefine regression test.

--- tests/testthat/test_remBoot.R
+++ tests/testthat/test_remBoot.R
@@ -4,6 +4,9 @@ test_that("remBoot produces a reasonable result", {
   # This is just a very basic regression test
   
   data(hDat)
+
+  # hDat has detection in kilometres, we want metres.
+  hDat$dist <- 1000 * hDat$dist
   tm <- 1800
   v <- 0.89
   nboots <- 1000
@@ -14,5 +17,5 @@ test_that("remBoot produces a reasonable result", {
   expect_length(output, 1)
   expect_named(output, "sd")
   expect_length(output$sd, 5)
-  expect_true(all(output$sd < 1))
+  expect_true(all(output$sd < 100))
 })
